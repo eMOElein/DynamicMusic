7de9f7ba2a0b48990a2f7bc692a5df2c974bf52a
eMOElein <aka_moe@gmx.de>
2023-09-25 22:26:08 +0200

    basic support for muse expanded dwemer

---
+++ scripts/DynamicMusic/soundBanks/1000_muse_expansion_dwemer.lua
@@ -0,0 +1,49 @@
+local soundBank =    {
+  id = "muse_expansion_dwemer",
+  cellNamePatterns = {
+    'Aleft',
+    'Arkngthand',
+    'Arkngthunch-Sturdumz',
+    'Bethamez',
+    'Bthanchend',
+    'Bthuand',
+    'Bthungthumz',
+    'Druscashti',
+    'Galom Daeus',
+    'Mudan',
+    'Mzahnch',
+    'Mzanchend',
+    'Mzuleft',
+    'Nchardahrk',
+    'Nchardumz',
+    'Nchuleft',
+    'Nchuleftingth',
+    'Nchurdamz',
+    'Bamz-Amschend',
+    'Sotha Sil'
+  },
+  tracks={
+    {
+      path="Music/MS/cell/Dwemer/exploration1.mp3",
+      length=209
+    },
+    {
+      path="Music/MS/cell/Dwemer/exploration2.mp3",
+      length=163
+    },
+    {
+      path="Music/MS/cell/Dwemer/exploration3.mp3",
+      length=213
+    },
+    {
+      path="Music/MS/cell/Dwemer/exploration4.mp3",
+      length=260
+    },
+    {
+      path="Music/MS/cell/Dwemer/exploration5.mp3",
+      length=158
+    }
+  }
+}
+
+return soundBank
