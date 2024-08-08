bffee386b99965617e0850a0bb49fd8c7ecd7d1b
PopeRigby <poperigby@mailbox.org>
2024-08-07 22:41:53 -0700

    Initial commit

---
+++ scripts/DynamicMusic/soundBanks/1100_muse_expansion_redoran_cells.lua
@@ -0,0 +1,35 @@
+local soundBank = {
+    id = 'muse_expansion_redoran',
+    cellNamePatterns = {
+        "Ald Velothi",
+        "Ald-ruhn",
+        "Bal Isra",
+        "Indarys Manor",
+        "Khuul",
+        "Maar Gan",
+    },
+    tracks = {
+        {
+            path = 'music/MS/cell/Redoran/exploration1.mp3',
+            length = 196
+        },
+        {
+            path = 'music/MS/cell/Redoran/exploration2.mp3',
+            length = 151
+        },
+        {
+            path = 'music/MS/cell/Redoran/exploration3.mp3',
+            length = 292
+        },
+        {
+            path = 'music/MS/cell/Redoran/exploration4.mp3',
+            length = 260
+        },
+        {
+            path = 'music/MS/cell/Redoran/exploration5.mp3',
+            length = 185
+        }
+    }
+}
+
+return soundBank
---
+++ scripts/DynamicMusic/soundBanks/1100_muse_expansion_redoran_enemies.lua
@@ -0,0 +1,29 @@
+local soundBank = {
+    id = 'muse_expansion_redoran',
+    enemyNames = {
+        "Redoran Guard",
+        "Bolvyn Venim",
+        "Minor Arobar",
+        "Garisa Llethri",
+        "Mistress Brara Morvayn",
+        "Hlaren Ramoran",
+        "Athyn Sarethi",
+        "Banden Indarys"
+    },
+    combatTracks = {
+        {
+            path = "music/MS/combat/Redoran/combat1.mp3",
+            length = 97
+        },
+        {
+            path = "music/MS/combat/Redoran/combat2.mp3",
+            length = 104
+        },
+        {
+            path = "music/MS/combat/Redoran/combat3.mp3",
+            length = 74
+        },
+    }
+}
+
+return soundBank
