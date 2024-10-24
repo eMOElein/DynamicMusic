e8af9889fc8a39e77389ae6a76cf43a74a6804da
eMOElein <aka_moe@gmx.de>
2024-10-24 20:24:33 +0200

    utils documentation

--- scripts/DynamicMusic/utils/StringUtils.lua
+++ scripts/DynamicMusic/utils/StringUtils.lua
@@ -1,5 +1,9 @@
 local StringUtils = {}
 
+--Splits a string by a separator and retruns the splitted strings in a new table.
+---@param string string The string to split.
+---@param separator string The separator that should be used for splitting.
+---@return table<string> splitted The nwe table with the splittet substrings.
 function StringUtils.split(string, separator)
     local t = {}
     for str in string.gmatch(string, "([^" .. separator .. "]+)") do
