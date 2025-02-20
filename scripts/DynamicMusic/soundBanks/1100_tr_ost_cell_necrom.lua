72ba0211221dd8778b65088fdb3c6da8b5b7e10b
Akakij S. Bzdrishchenko <ww.lss.su@gmail.com>
2025-02-20 23:13:50 +0500

    added Necrom playlist (the same as for Sacred Lands)

---
+++ scripts/DynamicMusic/soundBanks/1100_tr_ost_cell_necrom.lua
@@ -0,0 +1,26 @@
+local soundbank =      {
+  cellNamePatterns = {
+	'Necrom'
+  },
+  interiorOnly = true,
+  tracks = {
+    {
+      path="Music/MS/region/Sacred Lands Region/sacred lands 1.mp3",
+      length=255
+    },
+    {
+      path="Music/MS/region/Sacred Lands Region/sacred lands 2.mp3",
+      length=135
+    },
+    {
+      path="Music/MS/region/Sacred Lands Region/sacred lands 3.mp3",
+      length=79
+    },
+    {
+      path="Music/MS/region/Sacred Lands Region/sacred lands 4.mp3",
+      length=76
+    }
+  }
+}
+
+return soundbank
